d2774b97b69355b17f0324789c085068ea82e792
Anton Schirg <anton.schirg@posteo.de>
2017-04-12 14:51:34 +0200

    gdb.sh calls xargo

--- gdb.sh
+++ gdb.sh
@@ -7,4 +7,13 @@ set -e
 echo "Please run 'st-util' in another terminal window (you might need sudo)"
 echo ""
 
+XARGO_ARGS=
+
+if [ "${TARGET}" == "release" ]
+then
+    XARGO_ARGS="--release"
+fi
+
+xargo build ${XARGO_ARGS}
+
 arm-none-eabi-gdb -iex 'add-auto-load-safe-path .' -ex "tar ext :4242" -ex "load-reset" "target/stm32f7/${TARGET}/roaster"
